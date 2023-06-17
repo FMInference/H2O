5d4b2cceacb6c3cb8ca20bce961a26db3f6995b1
Allen <39230030+Kyriection@users.noreply.github.com>
2023-06-17 00:14:21 -0500

    fix accumulate score bug

--- h2o_hf/scripts/generation/opt_h2o.sh
+++ h2o_hf/scripts/generation/opt_h2o.sh
@@ -1,4 +1,4 @@
 python -u run_text_generation.py \
     --model_arch opt \
     --model_name facebook/opt-6.7b \
-    --recent_ratio 0.07 --heavy_ratio 0.07
+    --recent_ratio 0.1 --heavy_ratio 0.1
--- h2o_hf/scripts/generation/opt_local.sh
+++ h2o_hf/scripts/generation/opt_local.sh
@@ -1,4 +1,4 @@
 python -u run_text_generation.py \
     --model_arch opt \
     --model_name facebook/opt-6.7b \
-    --recent_ratio 0.14 --heavy_ratio 0
+    --recent_ratio 0.2 --heavy_ratio 0
