ff8343570c5aadb9b328906eb124ee29142d94d3
Allen <39230030+Kyriection@users.noreply.github.com>
2023-06-19 01:48:45 -0500

    Update run_text_generation.py

--- h2o_hf/run_text_generation.py
+++ h2o_hf/run_text_generation.py
@@ -127,15 +127,6 @@ def main():
     # prompt_text = 'In the year 2087, humanity has achieved remarkable technological advancements and established colonies on multiple planets within the Milky Way galaxy. Interstellar travel has become commonplace, with faster-than-light spacecraft enabling people to explore distant star systems. Earth has undergone significant changes due to sustainable development efforts, such as harnessing renewable energy sources and implementing widespread ecological restoration projects. However, alongside these triumphs, new challenges have emerged, including the rise of artificial intelligence, ethical dilemmas surrounding genetic engineering, and interplanetary political tensions. Against this backdrop, a team of intrepid scientists embarks on a mission to uncover the secrets of an ancient alien civilization, hidden deep within an uncharted exoplanet. As they navigate treacherous terrains and encounter otherworldly phenomena, they must confront their own fears and reconcile humanity\'s thirst for knowledge with the potential consequences of uncovering secrets that were better left buried. The fate of both their mission and the future of humanity hang in the balance.'
     prompt_text = 'In a small, bustling cafe nestled in the heart of a vibrant city, a serendipitous event unfolded, leaving a lasting impression on all who witnessed it. As the patrons sat sipping their coffees and engaging in animated conversations, a talented street musician entered the cafe, carrying a weathered guitar and radiating an aura of creativity.'
 
-
-
-
-
-
-
-
-
-
     model_name = args.model_name
     config = AutoConfig.from_pretrained(model_name, cache_dir=args.cache_dir)
     config.heavy_ratio = args.heavy_ratio
