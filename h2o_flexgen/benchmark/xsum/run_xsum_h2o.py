d809477388ae9c23044786ca3c43650b407a16a5
Ying Sheng <sqy1415@gmail.com>
2023-08-06 16:43:36 -0700

    Add xsum exp for h2o_flexgen (#1)

---
+++ h2o_flexgen/benchmark/xsum/run_xsum_h2o.py
@@ -0,0 +1,249 @@
+import argparse
+
+import numpy as np
+import torch
+import json
+import time
+from tqdm import tqdm 
+import copy 
+
+from flexgen.flex_opt import Policy, OptLM, ExecutionEnv, CompressionConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+
+
+MAX_LENGTH = int(10000)  # Hardcoded max length to avoid infinite loop
+
+
+def set_seed(args):
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+def adjust_length_to_model(length, max_sequence_length):
+    if length < 0 and max_sequence_length > 0:
+        length = max_sequence_length
+    elif 0 < max_sequence_length < length:
+        length = max_sequence_length  # No generation bigger than model size
+    elif length < 0:
+        length = MAX_LENGTH  # avoid infinite loop
+    return length
+
+
+def get_policy(model_name, hh_ratio, gbs, num_gb, percent=None):
+    gpu_batch_size = gbs
+    num_gpu_batches = num_gb
+    if model_name == "opt-6.7b":
+        cpu_cache_compute = False
+    elif model_name == "opt-30b":
+        cpu_cache_compute = True
+    else:
+        raise Exception("unsupported model")
+
+    policy = Policy(gpu_batch_size, num_gpu_batches,
+                    percent[0], percent[1],
+                    percent[2], percent[3],
+                    percent[4], percent[5],
+                    overlap=True, sep_layer=True, pin_weight=True,
+                    cpu_cache_compute=cpu_cache_compute, attn_sparsity=1.0,
+                    compress_weight=False,
+                    comp_weight_config=CompressionConfig(
+                        num_bits=4, group_size=64,
+                        group_dim=0, symmetric=False),
+                    compress_cache=False,
+                    comp_cache_config=CompressionConfig(
+                        num_bits=4, group_size=64,
+                        group_dim=2, symmetric=False),
+                    hh_ratio=hh_ratio,
+                    hh_all=True,
+                    )
+    return policy
+
+
+def get_batches(requests, policy):
+    batches = []
+    cpu_batch_size = policy.gpu_batch_size * policy.num_gpu_batches
+    num_batch = ((len(requests) - 1) // cpu_batch_size) + 1
+    print("number of batches", num_batch)
+    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b", padding_side="left")
+
+    prompt_len = 0
+    for i in range(num_batch):
+        batch = {"max_new_tokens": 0, "input_ids": [], "eos_token_id": None}
+
+        start_idx = i * cpu_batch_size
+        if i == num_batch - 1:
+            end_idx = len(requests)
+            num_gpu_batches = (end_idx - start_idx - 1) // policy.gpu_batch_size + 1
+            last_policy = Policy(policy.gpu_batch_size, num_gpu_batches,
+                                 policy.w_gpu_percent,
+                                 policy.w_cpu_percent,
+                                 policy.cache_gpu_percent,
+                                 policy.cache_cpu_percent,
+                                 policy.act_gpu_percent,
+                                 policy.act_cpu_percent,
+                                 overlap=True, sep_layer=True, pin_weight=True,
+                                 cpu_cache_compute=policy.cpu_cache_compute, attn_sparsity=1.0,
+                                 compress_weight=False,
+                                 comp_weight_config=CompressionConfig(
+                                     num_bits=4, group_size=64,
+                                     group_dim=0, symmetric=False),
+                                 compress_cache=False,
+                                 comp_cache_config=CompressionConfig(
+                                     num_bits=4, group_size=64,
+                                     group_dim=2, symmetric=False),
+                                 hh_ratio=policy.hh_ratio,
+                                 hh_all=True,
+                                 )
+            new_batch_size = last_policy.gpu_batch_size * last_policy.num_gpu_batches
+            for j in range(cpu_batch_size):
+                requests.append(requests[end_idx - 1])
+            end_idx = start_idx + new_batch_size
+            print("last batch index range", start_idx, end_idx)
+        else:
+            end_idx = (i + 1) * cpu_batch_size
+        assert (end_idx - start_idx) % policy.gpu_batch_size == 0
+
+        for req in requests[start_idx : end_idx]:
+            max_tokens = req["request"]["max_tokens"]
+            prompt = req["request"]["prompt"]
+            stop = req["request"]["stop"]
+            input_ids = tokenizer([prompt], add_special_tokens=False).input_ids[0]
+            # eos_token_id = tokenizer(stop).input_ids[0]
+
+            prompt_len = max(prompt_len, len(input_ids))
+            batch["max_new_tokens"] = max(batch["max_new_tokens"], max_tokens)
+            batch["input_ids"].append(input_ids)
+            # assert batch["eos_token_id"] is None or batch["eos_token_id"] == eos_token_id
+            # batch["eos_token_id"] = eos_token_id
+
+        batches.append(batch)
+
+    # manual padding
+    for batch in batches:
+        for j in range(len(batch["input_ids"])):
+            cnt = prompt_len - len(batch["input_ids"][j])
+            batch["input_ids"][j] = np.pad(batch["input_ids"][j], (cnt, 0), "constant", constant_values=(0))
+
+    return batches, last_policy
+
+
+def run_inference(model_name, requests, hh_ratio, gbs, num_gb, percent):
+    assert model_name == "opt-6.7b" or model_name == "opt-30b"
+
+    env = ExecutionEnv.create("~/flexgen_offload_dir")
+    policy = get_policy(model_name, hh_ratio, gbs, num_gb, percent)
+
+    batches, last_policy = get_batches(requests, policy)
+
+    print(f"Init weights begin.")
+    tic = time.time()
+    model = OptLM(model_name, env, "~/opt_weights", policy)
+    print(f"Init weights end. Elapsed: {time.time() - tic:.2f} s", flush=True)
+
+    # Generate
+    print(f"Generate begin.")
+    tic = time.time()
+    gen_tokens = 0
+    for i, batch in tqdm(enumerate(batches[:-1]), total=len(batches[:-1])):
+        input_ids = batch["input_ids"]
+        print("batch size:", len(input_ids))
+        print("prompt len:", len(input_ids[0]))
+        print("max new tokens:", batch["max_new_tokens"])
+        print("eos id:", batch["eos_token_id"])
+        output_ids = model.generate(
+            input_ids,
+            do_sample=True,
+            temperature=1e-7,
+            max_new_tokens=batch["max_new_tokens"],
+            stop=batch["eos_token_id"])
+        gen_tokens += len(input_ids) * batch["max_new_tokens"]
+        total_time = time.time() - tic
+        throughput = gen_tokens / total_time
+        print(f"Generation throughput: {throughput:.2f} token/s", flush=True)
+
+    # last batch
+    del model
+    model = OptLM(model_name, env, "~/opt_weights", last_policy)
+
+    tic = time.time()
+    batch = batches[-1]
+    input_ids = batch["input_ids"]
+    output_ids = model.generate(
+        input_ids,
+        do_sample=True,
+        temperature=1e-7,
+        max_new_tokens=batch["max_new_tokens"],
+        stop=batch["eos_token_id"])
+    gen_tokens += len(input_ids) * batch["max_new_tokens"]
+    total_time += time.time() - tic
+    throughput = gen_tokens / total_time
+
+    print(f"Generate end. Elapsed: {total_time:.2f} s", flush=True)
+    print(f"Generation throughput: {throughput:.2f} token/s", flush=True)
+
+    return total_time, throughput
+
+    # tokenizer = AutoTokenizer.from_pretrained("facebook/opt-30b", padding_side="left")
+    # outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[-100:]
+    # print("Sample Outputs:\n" + 70 * '-')
+    # #for i in range(len(outputs)):
+    # for i in [0, len(outputs) - 1]:
+    #     print(f"{i}:\n{outputs[i]}")
+    #     print("-" * 70)
+
+    # return outputs
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input_path", type=str, default="../../../h2o_hf/data/xsum_opt.jsonl")
+    parser.add_argument("--output_path", type=str, default="output")
+
+    parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--cache_dir", type=str, default="../../checkpoint/")
+
+    parser.add_argument("--hh_ratio", type=float, default=0.1)
+    parser.add_argument("--gbs", type=int, required=True)
+    parser.add_argument("--num_gb", type=int, required=True)
+    parser.add_argument("--percent", nargs="+", type=int, required=True)
+
+    parser.add_argument("--sample_num", type=int, default=1000)
+
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    args = parser.parse_args()
+
+    args.device = torch.device("cuda")
+    args.n_gpu = 1
+
+    set_seed(args)
+
+    model_name = args.model_name
+    input_path = args.input_path
+    output_path = args.output_path 
+
+    # load requests
+    requests = []
+    with open(input_path, 'r') as f:
+        for line in f:
+            if line.strip() != '':
+                requests.append(json.loads(line))
+    print("number of requests", len(requests))
+    if args.sample_num < len(requests):
+        print('Sample {} Examples'.format(args.sample_num))
+    requests = requests[:args.sample_num]
+    # requests = sorted(requests, key=lambda x: len(x["request"]["prompt"]))
+    # print([len(req["request"]["prompt"]) for req in requests])
+
+    # run inference
+    total_time, throughput = run_inference(model_name, requests, args.hh_ratio,
+                                           args.gbs, args.num_gb, args.percent)
+    with open(output_path, 'w') as f:
+        res = {"total time": total_time, "generation throughput": throughput}
+        json.dump(res, f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ h2o_flexgen/benchmark/xsum/xsum.sh
@@ -0,0 +1,40 @@
+# H2O 0.05 + 0.05
+python run_xsum_h2o.py --model opt-6.7b --output h2o_6.7b_0.05hh.res --hh_ratio 0.05 --gbs 1 --num_gb 1 --percent 100 0 100 0 100 0
+# python run_xsum_h2o.py --model opt-30b --output h2o_30b_0.05hh.res --hh_ratio 0.05 --gbs 3 --num_gb 35 --percent 20 80 0 100 0 100
+python ../../flexgen/flex_opt.py --model facebook/opt-30b --percent 20 80 0 100 0 100 --gpu-batch-size 3 --num-gpu-batches 35 --cpu --prompt-len 1978 --gen-len 64 --debug fewer_batch --hh-ratio 0.05 --hh-all
+
+# H2O 0.1 + 0.1
+python run_xsum_h2o.py --model opt-6.7b --output h2o_6.7b_0.1hh.res --hh_ratio 0.1 --gbs 1 --num_gb 1 --percent 100 0 100 0 100 0
+# python run_xsum_h2o.py --model opt-30b --output h2o_30b_0.1hh.res --hh_ratio 0.1 --gbs 3 --num_gb 15 --percent 20 80 0 100 0 100
+python ../../flexgen/flex_opt.py --model facebook/opt-30b --percent 20 80 0 100 0 100 --gpu-batch-size 3 --num-gpu-batches 15 --cpu --prompt-len 1978 --gen-len 64 --debug fewer_batch --hh-ratio 0.1 --hh-all
+
+# H2O 0.2 + 0.2
+python run_xsum_h2o.py --model opt-6.7b --output h2o_6.7b_0.2hh.res --hh_ratio 0.2 --gbs 1 --num_gb 1 --percent 80 20 100 0 100 0
+# python run_xsum_h2o.py --model opt-30b --output h2o_30b_0.2hh.res --hh_ratio 0.2 --gbs 3 --num_gb 10 --percent 20 80 0 100 0 100
+python ../../flexgen/flex_opt.py --model facebook/opt-30b --percent 20 80 0 100 0 100 --gpu-batch-size 3 --num-gpu-batches 10 --cpu --prompt-len 1978 --gen-len 64 --debug fewer_batch --hh-ratio 0.2 --hh-all
+
+# H2O 0.3 + 0.3
+python run_xsum_h2o.py --model opt-6.7b --output h2o_6.7b_0.3hh.res --hh_ratio 0.3 --gbs 1 --num_gb 1 --percent 80 20 100 0 100 0
+# python run_xsum_h2o.py --model opt-30b --output h2o_30b_0.3hh.res --hh_ratio 0.3 --gbs 3 --num_gb 7 --percent 20 80 0 100 0 100
+python ../../flexgen/flex_opt.py --model facebook/opt-30b --percent 20 80 0 100 0 100 --gpu-batch-size 3 --num-gpu-batches 7 --cpu --prompt-len 1978 --gen-len 64 --debug fewer_batch --hh-ratio 0.3 --hh-all
+
+# FlexGen (build original FlexGen rather than h2o_flexgen)
+python run_xsum_flexgen.py --model opt-6.7b --output flexgen_6.7b.res
+python run_xsum_flexgen.py --model opt-30b --output flexgen_30b.res
+
+# python run_xsum_flexgen.py --sample_num 10 --model opt-6.7b --output flexgen_6.7b.res
+# python run_xsum_flexgen.py --sample_num 10 --model opt-30b --output flexgen_30b.res
+
+# do not use: this is just a template for helm_run.py
+# opt-6.7b
+# time python3 helm_run.py --description summarization_xsum_sampled:model=text,temperature=0.3,device=cpu \
+#                          --model facebook/opt-6.7b\
+#                          --percent 0 100 0 100 0 100 \
+#                          --gpu-batch-size 8 --num-gpu-batches 4 --cpu \
+#                          --max-eval-instance 518
+# opt-30b
+# time python3 helm_run.py --description summarization_xsum_sampled:model=text,temperature=0.3,device=cpu \
+#                          --model facebook/opt-30b \
+#                          --percent 0 100 0 100 0 100 \
+#                          --gpu-batch-size 8 --num-gpu-batches 4 --cpu \
+#                          --max-eval-instance 518
